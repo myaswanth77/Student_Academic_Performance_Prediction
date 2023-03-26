c114f617965bf36cc28aae9aef6c9e6cf5a26aeb
M Yaswanth <yaswanthmotikavalasa@gmail.com>
2023-03-26 23:34:30 +0530

    This commit includes database connection

--- app.py
+++ app.py
@@ -1,31 +1,102 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template,redirect,url_for
+from flask import Flask, request, jsonify, render_template,redirect,url_for,flash
 import pickle
 import model
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+
 
 # Create flask app
 flask_app = Flask(__name__)
-# flask_app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Yash@709@localhost:5432/sapp_db'
-# db=SQLAlchemy(flask_app)
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sapp123@localhost:5432/sapp_db'
+flask_app.config['SECRET_KEY'] = secrets.token_hex(16)
+db = SQLAlchemy(flask_app)
 
-# class Login(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(50), unique=True, nullable=False)
-#     password = db.Column(db.String(50), nullable=False)
-
-# class Registration(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50), nullable=False)
-#     rollno = db.Column(db.String(50), unique=True, nullable=False)
-#     email = db.Column(db.String(50), unique=True, nullable=False)
-#     password = db.Column(db.String(50), nullable=False)
+# Create login manager
+login_manager = LoginManager()
+login_manager.init_app(flask_app)
+login_manager.login_view = 'login'
 
 
-# # Create database tables
-# with flask_app.app_context():
-#     db.create_all()
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True)  
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+
+
+
+# Define loader function for login manager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@flask_app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/grade')
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Logged in successfully')
+            return redirect('/grade')
+        else:
+            flash('Invalid credentials')
+            return redirect('/login')
+
+    else:
+        return render_template('login.html')
+    
+@flask_app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@flask_app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/grade')
+
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['rollno']
+        email = request.form['email']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if password1 == password2:
+            if User.query.filter_by(username=username).first():
+                flash('Username already exists')
+                return redirect('/register')
+
+            elif User.query.filter_by(email=email).first():
+                flash('Email already exists')
+                return redirect('/register')
+
+            else:
+                user = User(name=name, username=username, email=email, password=generate_password_hash(password1))
+                db.session.add(user)
+                db.session.commit()
+                flash('User created')
+                return redirect('/login')
+        else:
+            flash('Password is not matching')
+            return redirect('/register')
+
+    else:
+        return render_template('register.html')
+
 
 
 model = pickle.load(open("sapp_final_algo.pkl", "rb"))
@@ -34,45 +105,13 @@ model = pickle.load(open("sapp_final_algo.pkl", "rb"))
 def home():
     return render_template("start.html")
 
-# @flask_app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == "POST" and "name" in request.form and "rollno" in request.form and "email" in request.form and "password1" in request.form and "password2" in request.form:
-#         name = request.form['name']
-#         rollno = request.form['rollno']
-#         email = request.form['email']
-#         password1 = request.form['password1']
-#         password2 = request.form['password2']
-#         if password1 == password2:
-#             new_registration = Registration(name=name, rollno=rollno, email=email, password=password1)
-#             db.session.add(new_registration)
-#             db.session.commit()
-#             return "Registration data added to database"
-#         else:
-#             return "Passwords do not match"
-#     return render_template("register.html")
 
+@flask_app.route('/grade')
+def grade():
+    if not current_user.is_authenticated:
+        return redirect('/login')
 
-
-
-# @flask_app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == "POST" and "username" in request.form and "password" in request.form:
-#         username = request.form['username']
-#         password = request.form['password']
-#         new_login = Login(username=username, password=password)
-#         db.session.add(new_login)
-#         db.session.commit()
-#         return "Login data added to database"
-#     return render_template("login.html")
-
-
-
-
-
-
-@flask_app.route("/grade")
-def index():
-    return render_template("grade.html")
+    return render_template('grade.html')
 
 @flask_app.route("/predict", methods = ["POST"])
 def predict():
@@ -126,6 +165,12 @@ def predict():
 def result():
     prediction_text = request.args.get("prediction_text")
     return render_template("result.html", prediction_text=prediction_text)
+
+# Create database tables
+with flask_app.app_context():
+    db.create_all()
+
+
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
